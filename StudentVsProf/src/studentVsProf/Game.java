0e106a89a60149752e340c40304ea1629f73849a
defit0 <david.sonneborn00@gmail.com>
2022-12-19 16:04:12 +0100

    Create Classes

---
+++ StudentVsProf/src/studentVsProf/Game.java
@@ -0,0 +1,13 @@
+package studentVsProf;
+
+public class Game {
+
+	public static void main(String[] args) {
+
+		
+		
+		
+		
+	}
+
+}
---
+++ StudentVsProf/src/studentVsProf/Prof.java
@@ -0,0 +1,7 @@
+package studentVsProf;
+
+public class Prof {
+
+	
+
+}
