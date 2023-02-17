239915ef44d1778343819208252f37e2625df307
defit0 <david.sonneborn00@gmail.com>
2023-02-17 17:24:55 +0100

    Dokumentation erstellt

--- StudentVsProf/src/StudentenVsDozenten/sql/Client.java
+++ StudentVsProf/src/StudentenVsDozenten/sql/Client.java
@@ -14,32 +14,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-/*wichtige Infos und Links!
-https://falconbyte.net/java-mysql.php
-https://dev.mysql.com/downloads/connector/j/
-https://www.schattenbaum.net/php/verbindung.php
-
-CREATE TABLE benutzer (
-	name VARCHAR(50),
-    CONSTRAINT pkbenutzer PRIMARY KEY (name)
-);
-CREATE TABLE highscoreliste (
-	id int NOT NULL AUTO_INCREMENT,
-    name VARCHAR(50),
-    datum DATE,
-    score Integer,
-    CONSTRAINT pkhighscoreliste PRIMARY KEY (id),
-    CONSTRAINT fkhighscorelistebenutzer FOREIGN KEY (name) REFERENCES benutzer (name)
-);
-statement.executeUpdate("INSERT INTO benutzer (name) VALUES ('Patricia')");//Benutzer hinzufügen
-statement.executeUpdate("DELETE FROM `spieler` WHERE name = 'test';"); //nutzer löschen
-Statement stm = con.createStatement(); //benötigt für SELECT-Abfrage
-ResultSet rs = stm.executeQuery("SELECT * FROM highscoreliste;"); //SELECT Abfrage
-while(rs.next()){
-System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3) + " "   );	}
-"REPLACE INTO highscoreliste (name,datum,score) VALUES ('" + name + "','" + date+ "'," + score + ");"
-*/
-
 public class Client {
 
 	static Scanner s = new Scanner(System.in);
