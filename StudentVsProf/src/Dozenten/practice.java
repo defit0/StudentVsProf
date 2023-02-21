9dcef52eefabb4bd3fad75f3577a00ad8613d713
Sebastian Engl <sebastian.jonathan.engl@iem.thm.de>
2023-02-21 10:24:20 +0100

    AttackType überarbeitet standartfunktion eingeführt

--- StudentVsProf/src/Dozenten/practice.java
+++ StudentVsProf/src/Dozenten/practice.java
@@ -1,29 +1,12 @@
 package Dozenten;
 
-import Student.Genie;
-import Student.deaf;
-import Student.standart;
+
 import StudentenVsDozenten.Effekte.Effect;
 import StudentenVsDozenten.Effekte.SlowDown;
 
-public class practice implements attackType{
-
-	@Override
-	public Effect attacPattern(deaf dt, int damage, int length, int intensity) {
-		return standard(damage, length, intensity);
-	}
-
-	@Override
-	public Effect attacPattern(Genie dt, int damage, int length, int intensity) {
-		return standard(damage, length, intensity);
-	}
-
-	@Override
-	public Effect attacPattern(standart dt, int damage, int length, int intensity) {
-		return standard(damage, length, intensity);
-	}
+public class practice extends attackType{
 	
-	private Effect standard(int damage, int length, int intensity) {
+	protected Effect standard(int damage, int length, int intensity) {
 		return new SlowDown(damage, length, intensity);
 		
 	}
