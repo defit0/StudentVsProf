ed4b238a878093a77b83f6955fcf7f00c436d994
marku <marku@PC-Markus>
2023-03-31 13:49:30 +0200

    1 doz hinzufügen

--- StudentVsProf/src/StudentenVsDozenten/Auswahl/Team.java
+++ StudentVsProf/src/StudentenVsDozenten/Auswahl/Team.java
@@ -29,13 +29,11 @@ public class Team extends Dozent implements Visible{
 		team[0] = new Forscher(PlayingField.GameMap.getFieldIn(0, 400));
 		team[1] = new Forscher(PlayingField.GameMap.getFieldIn(0, 0));
 		team[2] = new Forscher(PlayingField.GameMap.getFieldIn(300, 100));
-<<<<<<< HEAD
 		team[3] = new TestDozent(PlayingField.GameMap.getFieldIn(200, 300));
 		 */
-=======
-		team[3] = new TestDozent(PlayingField.GameMap.getFieldIn(0, 300));
+		team.add(new TestDozent(PlayingField.GameMap.getFieldIn(200, 300)));
 
->>>>>>> branch 'master' of https://github.com/defit0/StudentVsProf
+
 	}
 	
 
